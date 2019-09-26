cb9cfbe7c16b3dda03796763bde069141557fef9
Enes Usta <enesusta@email.com>
2019-09-26 20:16:12 +0300

    @Max tests implemented

---
+++ src/main/java/net/enesusta/validator/max/MaxBean.java
@@ -0,0 +1,37 @@
+package net.enesusta.validator.max;
+
+public class MaxBean {
+
+    @Max(max = 15)
+    private int anInt;
+
+    @Max(max = 5)
+    private String string;
+
+    @Max(max = 4)
+    private float aFloat;
+
+    public int getAnInt() {
+        return anInt;
+    }
+
+    public void setAnInt(int anInt) {
+        this.anInt = anInt;
+    }
+
+    public String getString() {
+        return string;
+    }
+
+    public void setString(String string) {
+        this.string = string;
+    }
+
+    public float getaFloat() {
+        return aFloat;
+    }
+
+    public void setaFloat(float aFloat) {
+        this.aFloat = aFloat;
+    }
+}
--- src/test/java/net/enesusta/validator/max/MaxValidatorTest.java
+++ src/test/java/net/enesusta/validator/max/MaxValidatorTest.java
@@ -1,12 +1,47 @@
 package net.enesusta.validator.max;
 
+import net.enesusta.validator.core.Validator;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class MaxValidatorTest {
 
+    private Validator validator;
+
     @BeforeEach
     void before() {
+        validator = new MaxValidator();
+    }
 
+    @Test
+    void valid_max_int() throws IllegalAccessException {
+        MaxBean bean = new MaxBean();
+        bean.setAnInt(14);
+        assertTrue(validator.isValid(bean));
+        bean.setAnInt(17);
+        assertFalse(validator.isValid(bean));
+    }
+
+    @Test
+    void valid_max_string() throws IllegalAccessException {
+        MaxBean bean = new MaxBean();
+        bean.setString("1343");
+        assertTrue(validator.isValid(bean));
+        bean.setString("1321132");
+        assertFalse(validator.isValid(bean));
+    }
+
+    @Test
+    void valid_max_floating() throws IllegalAccessException {
+        MaxBean bean = new MaxBean();
+        bean.setaFloat(2.3f);
+        assertTrue(validator.isValid(bean));
+        bean.setaFloat(8.3f);
+        assertFalse(validator.isValid(bean));
+        bean.setAnInt(1321);
+        assertFalse(validator.isValid(bean));
     }
 
 }
