e88d145910cfb712658a37cb120053afa510a52b
Enes Usta <enesusta@email.com>
2019-10-08 21:23:56 +0300

    Checkstyle plugin added

--- src/main/java/net/enesusta/validator/core/AnnotationValidator.java
+++ src/main/java/net/enesusta/validator/core/AnnotationValidator.java
@@ -10,31 +10,39 @@ import net.enesusta.validator.size.Size;
 
 import java.lang.reflect.Field;
 
+@SuppressWarnings("checkstyle:LineLength")
 public interface AnnotationValidator {
+    @SuppressWarnings("checkstyle:MissingJavadocMethod")
     default boolean isAnnotationPresentWithNonNullAnnotation(final Field field) {
         return field.isAnnotationPresent(NonNull.class);
     }
 
+    @SuppressWarnings("checkstyle:MissingJavadocMethod")
     default boolean isAnnotationPresentWithPositiveAnnotation(final Field field) {
         return field.isAnnotationPresent(Positive.class);
     }
 
+    @SuppressWarnings("checkstyle:MissingJavadocMethod")
     default boolean isAnnotationPresentWithSizeAnnotation(final Field field) {
         return field.isAnnotationPresent(Size.class);
     }
 
+    @SuppressWarnings("checkstyle:MissingJavadocMethod")
     default boolean isAnnotationPresentWithMinAnnotation(final Field field) {
         return field.isAnnotationPresent(Min.class);
     }
 
+    @SuppressWarnings("checkstyle:MissingJavadocMethod")
     default boolean isAnnotationPresentWithMaxAnnotation(final Field field) {
         return field.isAnnotationPresent(Max.class);
     }
 
+    @SuppressWarnings("checkstyle:MissingJavadocMethod")
     default boolean isAnnotationPresentWithNegativeAnnotation(final Field field) {
         return field.isAnnotationPresent(Negative.class);
     }
 
+    @SuppressWarnings("checkstyle:MissingJavadocMethod")
     default boolean isAnnotationPresentWithEmailAnnotation(final Field field) {
         return field.isAnnotationPresent(Email.class);
     }
--- src/main/java/net/enesusta/validator/positive/PositiveFieldValidator.java
+++ src/main/java/net/enesusta/validator/positive/PositiveFieldValidator.java
@@ -1,19 +1,35 @@
 package net.enesusta.validator.positive;
 
 import net.enesusta.validator.core.FieldValidator;
-import net.enesusta.validator.nonnull.NonNull;
-
 import java.lang.reflect.Field;
-import java.util.Optional;
-import java.util.stream.IntStream;
+
+/**
+ * @author Enes Usta
+ * @see net.enesusta.validator.core.FieldValidator
+ * @since 0.0.1
+ */
 
 public class PositiveFieldValidator implements FieldValidator {
 
+
     private Object object;
 
+    /**
+     *
+      * @param object a
+     */
+
+    @SuppressWarnings("checkstyle:HiddenField")
     public PositiveFieldValidator(final Object object) {
         this.object = object;
     }
+
+    /**
+     *
+     * @param field
+     * @return
+     * @throws IllegalAccessException
+     */
 
     @Override
     public final boolean isFieldValid(final Field field) throws IllegalAccessException {
@@ -33,13 +49,27 @@ public class PositiveFieldValidator implements FieldValidator {
         return valid;
     }
 
-    private <E extends Number> boolean isPositive(E e, final Field field) {
+    /**
+     * @param e e
+     * @param field a
+     * @param <E> a
+     * @return boolean
+     */
+
+    private <E extends Number> boolean isPositive(final E e, final Field field) {
         int value = e.intValue();
         result(field.getName(), value, value >= 0);
         return value >= 0;
     }
 
-    private void result(String name, int a, boolean b) {
+    /**
+     *
+     * @param name
+     * @param a
+     * @param b
+     *
+     */
+    private void result(final String name, final int a, final boolean b) {
         System.out.println("\n ===================== ");
         System.out.println(name + " = " + a + " isValid = " + b);
         System.out.println(" ===================== ");
